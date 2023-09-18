662801365438a2c747739ff11190310dbfbbafc6
Sebaru18 <89612368+Sebaru18@users.noreply.github.com>
2023-09-18 15:29:06 -0500

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,59 @@
+package com.example.demoarquitectura.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String descripcion;
+    private Long precio;
+    private int stock;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Long precio) {
+        this.precio = precio;
+    }
+
+    public int getStock(){return stock;}
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
