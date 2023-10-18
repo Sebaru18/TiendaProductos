f28108ca678b9ab34bcf2578f007c2758c38de88
Sebastian Tovar <80776351+sebastiantovarp@users.noreply.github.com>
2023-10-18 15:24:26 -0500

    Delete TiendaProductos directory

--- TiendaProductos/src/main/java/com/example/tiendaproductos/repository/crud/ProductCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tiendaproductos.repository.crud;
-
-import com.example.tiendaproductos.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductCrudRepository extends CrudRepository<Product,Integer> {
-}
